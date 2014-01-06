2e141112045f9f255e547eb0be87ce9a735df827
Mark Rada <marada@uwaterloo.ca>
2014-01-06 00:55:36 -0500

    Implement the test wrapper for the marmoset tests

--- test/helper.rb
+++ test/helper.rb
@@ -3,6 +3,7 @@ require 'simplecov'
 
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'open3'
 
 ##
 # Joos test suite extensions to Minitest
@@ -10,6 +11,52 @@ class Minitest::Test
 
   def fixture name
     File.expand_path('./test/fixtures/' + name)
+  end
+
+  def lexical_analysis *files
+    Open3.capture3("./bin/joosc #{files.join(' ')}")
+  end
+
+  def assert_analysis *files
+    stdout, stderr, status = lexical_analysis(*files)
+    assert(status.success?) {
+      <<-EOM
+STATUS: #{status.exitstatus}
+
+STDOUT
+======
+#{stdout}
+
+STDERR
+======
+#{stderr}
+
+INPUT
+=====
+#{files.map { |f| f + ":\n" + File.read(f)}.join("\n\n")}
+     EOM
+    }
+  end
+
+  def refute_analysis *files
+    stdout, stderr, status = lexical_analysis(*files)
+    assert_equal(42, status.exitstatus) {
+      <<-EOM
+STATUS: #{status.exitstatus}
+
+STDOUT
+======
+#{stdout}
+
+STDERR
+======
+#{stderr}
+
+INPUT
+=====
+#{files.map { |f| f + ":\n" + File.read(f)}.join("\n\n")}
+     EOM
+    }
   end
 
 end
