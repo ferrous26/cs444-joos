bf30aa3fbd50934b59f00ad33cfdba99ea939245
Mark Rada <marada@uwaterloo.ca>
2014-01-14 22:53:03 -0500

    More pending specs for Literals!

--- spec/token/literal_spec.rb
+++ spec/token/literal_spec.rb
@@ -12,29 +12,33 @@ describe Joos::Token::Literal do
     it 'is a ConstantToken'
     it 'returns the binary representation from #to_binary'
     it 'registers itself with CONSTANT_TOKENS'
+    it 'includes ConstantToken'
   end
 
   describe Joos::Token::False do
     it 'is a ConstantToken'
     it 'returns the binary representation from #to_binary'
     it 'registers itself with CONSTANT_TOKENS'
+    it 'includes ConstantToken'
   end
 
   describe Joos::Token::Null do
     it 'is a ConstantToken'
     it 'returns the binary representation from #to_binary'
     it 'registers itself with CONSTANT_TOKENS'
+    it 'includes ConstantToken'
   end
 
   describe Joos::Token::Int do
     it 'raises an error if the value is outside of allowed ranges'
-    it 'returns a 32-bit binary representation from #to_binary'
     it 'registers itself with PATTERN_TOKENS'
+    it 'returns a 32-bit binary representation from #to_binary'
+    it 'returns the Fixnum value via #to_i'
   end
 
   describe Joos::Token::Float do
     it 'is an IllegalToken'
-    it 'registers itself with PATTERN_TOKENS'
+    it 'registers itself with PATTERN_TOKENS' # ?
   end
 
   describe Joos::Token::String do
@@ -42,12 +46,17 @@ describe Joos::Token::Literal do
     it 'returns the binary representation from #to_binary'
     it 'maintains a global array of all strings and avoids duplication'
     it 'registers itself with PATTERN_TOKENS'
+    it 'validates all character escape sequences'
+    it 'validates all octal escape sequences'
   end
 
   describe Joos::Token::Char do
     it 'returns the binary representation from #to_binary'
     it 'maintains a global array of all chars and avoids duplication'
     it 'registers itself with PATTERN_TOKENS'
+    it 'validates all character escape sequences'
+    it 'validates all octal escape sequences'
+    it 'ensures that the length of the character string is one'
   end
 
 end
