06577c43cfbb4a242fcaedc224371eecc30b5e21
Mark Rada <marada@uwaterloo.ca>
2014-01-29 15:51:56 -0500

    Add a file to contain utility methods

---
+++ lib/joos/utilities.rb
@@ -0,0 +1,29 @@
+require 'joos/version'
+
+##
+# Misc. utility methods used by the compiler.
+module Joos::Utilities
+  extend self
+
+  ##
+  # Figure out how many CPU cores are available on the host system
+  #
+  # Since figuring this value out will vary depending on the platform,
+  # this method is not always dependable. It has been tested to work
+  # on OS X and the Linux environment provided by the university.
+  #
+  # @example
+  #
+  #   Joos::Utilities.number_of_cpu_cores # => 8  # my laptop
+  #   Joos::Utilities.number_of_cpu_cores # => 48 # abeast machine
+  #
+  # @return [Fixnum]
+  def number_of_cpu_cores
+    @cpus ||= (if system('which nproc')
+                 `nproc`
+               else
+                 `sysctl hw.ncpu | awk '{print $2}'`
+               end).to_i
+  end
+
+end
