d9af8322bf6b682c74f927c8c12228fa318ea952
Mark Rada <marada@uwaterloo.ca>
2014-01-29 16:29:31 -0500

    Fill out the interface for the Compiler to the scanner and parser

--- lib/joos.rb
+++ lib/joos.rb
@@ -1,4 +1,5 @@
 require 'joos/version'
+require 'joos/utilities'
 # require 'joos/scanner'
 # require 'joos/parser'
 
@@ -25,9 +26,19 @@ class Joos::Compiler
   # @return [Array<String>]
   attr_reader :files
 
+  ##
+  # The result code that the Joos front end should exit with
+  #
+  # The return value will be either {SUCCESS} or {ERROR} depending on
+  # if there were problems during compilation or not.
+  #
+  # @return [Fixnum]
+  attr_reader :result
+
   # @param files [Array<String>]
   def initialize *files
-    @files = files.flatten
+    @files  = files.flatten
+    @result = SUCCESS
   end
 
   ##
@@ -36,19 +47,38 @@ class Joos::Compiler
   # For each {#files}, a `.s` file will be created with the appropriate
   # assembly code.
   def compile
-    @files.each do |file|
+    q = Queue.new
+    @files.each do |file| q.push file end
+
+    thread_count = [Joos::Utilities.number_of_cpu_cores, @files.size].min
+    threads      = Array.new(thread_count) do make_scan_and_parse_job(q) end
+    thread_count.times do q.push nil end
+    threads.each(&:join)
+  end
+
+
+  private
+
+  # @param q [Queue]
+  def make_scan_and_parse_job q
+    Thread.new do
+      loop do
+        job = q.pop
+        break unless job
+        scan_and_parse job
+      end
     end
   end
 
-  ##
-  # The result code that the Joos front end should exit with
-  #
-  # The return value will be either {SUCCESS} or {ERROR} depending on
-  # if there were problems during compilation or not.
-  #
-  # @return [Fixnum]
-  def result
-    File.basename(@files.first, '.java').match(/\AJe/) ? ERROR : SUCCESS
+  # @param job [String] path to the file to work on
+  def scan_and_parse job
+    input = File.readlines(job)
+#    raise 'joosc only accepts ASCII input' unless input.all?(&:ascii_only?)
+    Joos::Parser.new(Joos::Scanner.new(input).consume_input).parse
+  rescue => e
+    $stderr.puts e.message
+    $stderr.puts e.backtrace if $DEBUG # used internally
+    @result = ERROR
   end
 
 end
--- spec/joos_spec.rb
+++ spec/joos_spec.rb
@@ -21,7 +21,22 @@ describe Joos::Compiler do
     expect(c).to respond_to :result
   end
 
-  it 'responds reasonably when a file path is incorrect'
+  it 'responds reasonably when a file path is incorrect' do
+    c = Joos::Compiler.new('herpDerp.java')
+    hack = $stderr
+    $stderr = StringIO.new
+    c.compile
+    expect(c.result).to be == Joos::Compiler::ERROR
+    expect($stderr.string).to match(/no such file or directory/i)
+    $stderr = hack
+  end
+
+  it 'does not allow exceptions to crash the program' do
+    c = Joos::Compiler.new('herpDerp.java')
+    expect {
+      c.compile
+    }.to_not raise_error
+  end
 
   it 'defines error and success correctly' do
     expect(Joos::Compiler::SUCCESS).to be == 0
@@ -29,6 +44,11 @@ describe Joos::Compiler do
   end
 
   it 'reports a SUCCESS result for successful compilation'
-  it 'reports an ERROR result for compilation failure cases'
+
+  it 'reports an ERROR result for compilation failure cases' do
+    c = Joos::Compiler.new('herpDerp.java')
+    c.compile
+    expect(c.result).to be == Joos::Compiler::ERROR
+  end
 
 end
