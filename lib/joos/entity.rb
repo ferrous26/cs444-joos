64db72d30eaa4ac50e5ef3de53aecb8e88a707a9
Mark Rada <marada@uwaterloo.ca>
2014-02-02 22:36:16 -0500

    Flesh out handling of many of the error cases for Joos::Entity objects
    With specs. Entity specific errors still need tests.

--- lib/joos/entity.rb
+++ lib/joos/entity.rb
@@ -13,10 +13,10 @@ class Joos::Entity
   ##
   # The canonical name of the entity
   #
-  # @return [Joos::Token::Identifier]
+  # @return [Joos::Token::Identifier, Joos::AST::QualifiedIdentifier]
   attr_reader :name
 
-  # @param name [Joos::AST::QualifiedIdentifier]
+  # @param name [Joos::Token::Identifier]
   def initialize name
     @name = name
   end
@@ -49,7 +49,7 @@ class Joos::Entity
     # Error raised when the name of the class/interface does not match the
     # name of the file.
     #
-    class EntityNameDoesNotMatchFileNameError < Exception
+    class NameDoesNotMatchFileError < Exception
       # @param unit [CompilationUnit]
       def initialize unit
         super "#{unit.name.value} does not match file name #{unit.name.file}"
@@ -68,9 +68,86 @@ class Joos::Entity
     # Joos source files require that any compilation units in the file have
     # the same name as the file itself.
     #
-    def ensure_that_class_name_matches_file_name
-      raise EntityNameDoesNotMatchFileNameError.new(self) unless
+    def ensure_unit_name_matches_file_name
+      raise NameDoesNotMatchFileError.new(self) unless
         File.basename(name.file, '.java') == name.value
+    end
+  end
+
+  ##
+  # Code common to all entities which can have modifiers.
+  #
+  module Modifiable
+    ##
+    # Exception raised when an entity is declared with duplicated modifiers
+    class DuplicateModifier < Exception
+      # @param entity [Joos::Entity]
+      def initialize entity
+        super "#{entity} is being declared with duplicate modifiers"
+      end
+    end
+
+    ##
+    # Exception raised when an entity uses a modifier that it is not allowed
+    # to use.
+    class InvalidModifier < Exception
+      # @param entity   [Joos::Entity]
+      # @param modifier [Joos::Token::Modifier]
+      def initialize entity, modifier
+        klass = entity.class.to_s.split('::').last
+        super "A #{klass} cannot use the #{modifier} modifier"
+      end
+    end
+
+    ##
+    # Exception raised when 2 modifiers which are mutually exclusive for an
+    # entity are applied to the same entity.
+    #
+    class MutuallyExclusiveModifiersError < Exception
+      # @param entity [Joos::Entity]
+      # @param mods [Array(Joos::Token::Modifier, Joos::Token::Modifier)]
+      def initialize entity, mods
+        super "#{entity} cannot be both #{mods.first} and #{mods.last}"
+      end
+    end
+
+
+    ##
+    # Modifiers of the receiver.
+    #
+    # @return [Array<Joos::Token::Modifier>]
+    attr_reader :modifiers
+
+    # @param name [Joos::AST::QualifiedIdentifier]
+    # @param modifiers [Array<Joos::Token::Modifier>]
+    def initialize name, modifiers
+      super name
+      @modifiers = modifiers
+    end
+
+
+    private
+
+    def ensure_no_duplicate_modifiers
+      uniq_size = modifiers.map(&:to_sym).uniq.size # fuuuuu
+      raise DuplicateModifier.new(self) unless uniq_size == modifiers.size
+    end
+
+    def ensure_modifiers_not_present *mods
+      mods.each do |mod|
+        raise InvalidModifier.new(self, mod) if modifiers.include? mod
+      end
+    end
+
+    # @param mods [Array(Joos::Token::Modifier, Joos::Token::Modifier)]
+    #   Only ever pass 2 modifiers to this method
+    def ensure_mutually_exclusive_modifiers *mods
+      raise MutuallyExclusiveModifiersError.new(self, mods) if
+        modifiers.include?(mods.first) && modifiers.include?(mods.last)
+    end
+
+    def ensure_only_one_visibility_modifier
+      ensure_mutually_exclusive_modifiers(:public, :protected)
     end
   end
 
@@ -124,12 +201,16 @@ class Joos::Entity
   # so it can be used to access those references as well.
   class Class < self
     include CompilationUnit
+    include Modifiable
 
     ##
-    # Modifiers on the receiver.
-    #
-    # @return [Joos::Token::Modifier]
-    attr_reader :modifiers
+    # Exception raised when a class has no explicit constructors
+    class NoConstructorError < Exception
+      # @param klass [Joos::Entity::Class]
+      def initialize klass
+        super "#{klass} must include at least one explicit constructor"
+      end
+    end
 
     ##
     # The superclass of the receiver.
@@ -167,10 +248,9 @@ class Joos::Entity
     # @param extends    [Class, nil]
     # @param implements [Array<Interface>]
     def initialize modifiers, name, extends, implements
-      super name
-      @modifiers    = modifiers
-      @extends      = extends
-      @implements   = implements
+      super name, modifiers
+      @extends      = extends  # || Joos::Core::Object
+      @implements   = implements || []
       @constructors = []
       @members      = []
     end
@@ -186,9 +266,11 @@ class Joos::Entity
     end
 
     def validate
+      ensure_that_unit_name_matches_file_name
+      ensure_no_duplicate_modifiers
+      ensure_modifiers_not_present(:private, :native, :static)
+      ensure_mutually_exclusive_modifiers(:final, :abstract)
       ensure_at_least_one_constructor
-      ensure_not_both_final_and_abstract
-      ensure_that_class_name_matches_file_name
       constructors.each(&:validate)
       members.each(&:validate)
     end
@@ -198,12 +280,6 @@ class Joos::Entity
 
     def ensure_at_least_one_constructor
       raise NoConstructorsError.new(self) if constructors.empty?
-    end
-
-    def ensure_not_both_final_and_abstract
-      raise FinalAbstractClassError.new(self) if
-        modifiers.include?(Joos::Token::Final) &&
-        modifiers.include?(Joos::Token::Abstract)
     end
   end
 
@@ -217,19 +293,14 @@ class Joos::Entity
   #
   class Interface < self
     include CompilationUnit
-
-    ##
-    # Modifiers on the receiver.
-    #
-    # @return [Joos::Token::Modifier]
-    attr_reader :modifiers
+    include Modifiable
 
     ##
     # The superclass of the receiver.
     #
-    # @return [Joos::Entity::Class]
+    # @return [Array<Joos::Entity::Interface>]
     attr_reader :extends
-    alias_method :superclass, :extends
+    alias_method :superinterfaces, :extends
 
     ##
     # All fields and methods defined on the class.
@@ -244,10 +315,9 @@ class Joos::Entity
     # @param name      [Joos::AST::QualifiedIdentifier]
     # @param extends   [Array<Joos::Token::Modifier>]
     def initialize modifiers, name, extends
-      super name
-      @modifiers = modifiers
-      @extends   = extends
-      @members   = []
+      super name, modifiers
+      @extends = extends || []
+      @members = []
     end
 
     # @param member [Method]
@@ -256,19 +326,10 @@ class Joos::Entity
     end
 
     def validate
-      ensure_that_class_name_matches_file_name
-      ensure_methods_are_not_static_final_or_native
+      ensure_that_unit_name_matches_file_name
+      ensure_no_duplicate_modifiers
+      ensure_modifiers_not_present(:protected, :final, :native, :static)
       members.each(&:validate)
-    end
-
-
-    private
-
-    def ensure_methods_are_not_static_final_or_native
-      mods = [Joos::Token::Static, Joos::Token::Final, Joos::Token::Native]
-      members.each do |member|
-        raise IllegalModifier.new(self) unless (member.modifiers & mods).empty?
-      end
     end
   end
 
@@ -276,36 +337,127 @@ class Joos::Entity
   # Entity representing the definition of an class/interface field.
   #
   class Field < self
-    # @return [Array<Joos::Token::Modifier>]
-    attr_reader :modifiers
+    include Modifiable
 
-    # @return [Array<Class, Interface, Joos::Primitive>]
+    # @return [Class, Interface, Joos::Token::Type]
     attr_reader :type
+
+    # @param modifiers [Array<Joos::Token::Modifier>]
+    # @param type      [Class, Interface, Joos::Token::Type]
+    # @param name      [Joos::Token::Identifier]
+    def initialize modifiers, type, name
+      super name, modifiers
+      @type = type
+    end
+
+    # @return [self]
+    def to_member
+      self
+    end
+
+    def validate
+      ensure_no_duplicate_modifiers
+      ensure_only_one_visibility_modifier
+    end
   end
 
   ##
   # Entity representing the definition of an class/interface method.
   #
   class Method < self
-    # @return [Joos::Token::Modifiers]
-    attr_reader :modifiers
-    # @return [Class, Interface, Joos::Primitive]
+    include Modifiable
+
+    # @return [Class, Interface, Joos::Token::Type]
     attr_reader :type
+
+    # @return [Joos::AST::MethodBody, nil]
+    attr_reader :body
+
+    # @param modifiers [Array<Joos::Token::Modifier>]
+    # @param type      [Joos::Token::Type]
+    # @param name      [Joos::AST::Identifier]
+    # @param body      [Joos::AST::MethodBody]
+    def initialize modifiers, type, name, body
+      super name, modifiers
+      @type = type
+      @body = body
+    end
+
+    # @return [self]
+    def to_member
+      self
+    end
+
+    def validate
+      ensure_no_duplicate_modifiers
+      ensure_only_one_visibility_modifier
+      ensure_mutually_exclusive_modifiers(:abstract, :static)
+      ensure_mutually_exclusive_modifiers(:abstract, :final)
+      ensure_native_method_is_static
+      ensure_body_presence_if_required
+    end
+
+
+    private
+
+    def ensure_body_presence_if_required
+      no_body = [:abstract, :native]
+      if (modifiers & no_body) == no_body
+        raise UnexpectedBodyError.new(self) if body
+      else
+        raise ExpectedBodyError.new(self) unless body
+      end
+    end
+
+    def ensure_native_method_is_static
+      if modifier_names.include? :native
+        raise NonStaticNativeMethodError.new(self) unless
+          modifier_names.include? :static
+      end
+    end
+  end
+
+  ##
+  # Specialization of the {Method} entity for interfaces.
+  #
+  # Interfaces impose extra restrictions on regular methods, so we
+  # need to have a specialized class to perform extra checks.
+  #
+  class InterfaceMethod < Method
+    def validate
+      super
+      ensure_modifiers_not_present(:protected, :static, :final, :native)
+    end
   end
 
   ##
   # Entity representing the declaration of a method parameter.
   #
-  class Parameter < self
+  class FormalParameter < self
+    # @return [Class, Interface, Joos::Token::Type]
     attr_reader :type
+
+    # @param type [Joos::Token::QualifiedIdentifier, Joos::Token::Type]
+    # @param name [Joos::Token::Identifier]
+    def initialize type, name
+      super name
+      @type = type # @todo resolve type?
+    end
   end
 
   ##
   # Entity representing the declaration of a local variable.
   #
   class LocalVariable < self
-    # @return [Class, Interface, Joos::Primitive]
+    # @return [Class, Interface, Joos::Token::Type]
     attr_reader :type
+
+    # @param type [Joos::Token::QualifiedIdentifier, Joos::Token::Type]
+    # @param name [Joos::Token::Identifier]
+    def initialize type, name
+      super name
+      @type = type
+    end
   end
 
   ##
@@ -314,8 +466,28 @@ class Joos::Entity
   # In Joos, interfaces cannot have constructors.
   #
   class Constructor < self
-    # @return [Array<Joos::Token::Modifier>]
-    attr_reader :modifiers
+    include Modifiable
+
+    # @return [Joos::AST::MethodBody]
+    attr_reader :body
+
+    # @param modifiers [Array<Joos::Token::Modifier>]
+    # @param name [Joos::Token::Identifier, Joos::AST::QualifiedIdentifier]
+    # @param body [Joos::AST::MethodBody]
+    def initialize modifiers, name, body
+      super name, modifiers
+      @body = body
+    end
+
+    # @return [self]
+    def to_constructor
+      self
+    end
+
+    def validate
+      ensure_only_one_visibility_modifier
+      ensure_modifiers_not_present(:static, :abstract, :final, :native)
+    end
   end
 
 end
--- spec/entity_spec.rb
+++ spec/entity_spec.rb
@@ -30,13 +30,151 @@ describe Joos::Entity do
     expect(e.name).to be == 'hi'
   end
 
+  it 'provides nice trace info via #to_s' do
+    token  = Joos::Token::Identifier.new('canopy', 'roof.java', 1, 3)
+    entity = Joos::Entity.new(token)
+    expect(entity.to_s).to be == 'Entity:canopy @ roof.java:1'
+  end
+
   it 'raises an error #validate' do
     expect {
       Joos::Entity.new('hi').validate
     }.to raise_error NotImplementedError
   end
 
+  describe Joos::Entity::CompilationUnit do
+    class CUTest
+      include Joos::Entity::CompilationUnit
+      attr_reader :name
+      def initialize file
+        @name = Object.new
+        @name.define_singleton_method(:file)  { file }
+        @name.define_singleton_method(:value) { 'CUTest' }
+      end
+    end
+
+    it 'responds to #compilation_unit with self' do
+      t = CUTest.new ''
+      expect(t.to_compilation_unit).to be t
+    end
+
+    it 'raises an exception when ensure_unit_name_matches_file_name fails' do
+      expect {
+        CUTest.new('test.java').send(:ensure_unit_name_matches_file_name)
+      }.to raise_error 'CUTest does not match file name test.java'
+    end
+
+    it 'does not raise an exception if unit_name_matches_file_name' do
+      expect {
+        CUTest.new('CUTest.java').send(:ensure_unit_name_matches_file_name)
+      }.to_not raise_error
+    end
+  end
+
+  describe Joos::Entity::Modifiable do
+    class MTestSuper
+      def initialize x
+      end
+    end
+    class MTest < MTestSuper
+      include Joos::Entity::Modifiable
+      def initialize tos, *mods
+        super 'name', mods
+        define_singleton_method(:to_s) { tos }
+      end
+    end
+
+    it 'raises an exception if any modifier has been duplicated' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Public.new('public', 'hi', 1, 20)]
+      expect {
+        MTest.new('elephant', *mods).send(:ensure_no_duplicate_modifiers)
+      }.to raise_error 'elephant is being declared with duplicate modifiers'
+    end
+
+    it 'does not raise an exception if there are no duplicate modifiers' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Protected.new('protected', 'hi', 1, 20)]
+      expect {
+        MTest.new('elephant', *mods).send(:ensure_no_duplicate_modifiers)
+      }.to_not raise_error
+    end
+
+    it 'raises an error when disallowed modifiers are used' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Static.new('static', 'hi', 1, 20),
+              Joos::Token::Final.new('final', 'hi', 1, 50)]
+      expect {
+        MTest.new('rhino', *mods).send(:ensure_modifiers_not_present, :static)
+      }.to raise_error 'A MTest cannot use the static modifier'
+    end
+
+    it 'does not raise an error when disallowed modifiers are not used' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Final.new('final', 'hi', 1, 50)]
+      expect {
+        MTest.new('rhino', *mods).send(:ensure_modifiers_not_present, :static)
+      }.to_not raise_error
+    end
+
+    it 'raises an error if mutually exclusive modifiers are used' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Static.new('static', 'hi', 1, 20),
+              Joos::Token::Final.new('final', 'hi', 1, 50)]
+      expect {
+        MTest.new('tiger', *mods).send(:ensure_mutually_exclusive_modifiers,
+                                       :static,
+                                       :final)
+      }.to raise_error 'tiger cannot be both static and final'
+    end
+
+    it 'does not raise an error if mutually exclusive modifiers are NOT used' do
+      [
+       [Joos::Token::Public.new('public', 'hi', 1, 3)],
+
+       [Joos::Token::Public.new('public', 'hi', 1, 3),
+        Joos::Token::Static.new('static', 'hi', 1, 3)],
+
+       [Joos::Token::Public.new('public', 'hi', 1, 3),
+        Joos::Token::Final.new('static', 'hi', 1, 3)]
+      ].each do |mods|
+        expect {
+          MTest.new('tiger', *mods).send(:ensure_mutually_exclusive_modifiers,
+                                         :static,
+                                         :final)
+        }.to_not raise_error
+      end
+    end
+
+    it 'adds a check to make sure only one visibility modifier is ever used' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Protected.new('protected', 'hi', 1, 3)]
+      expect {
+        MTest.new('bear', *mods).send(:ensure_only_one_visibility_modifier)
+      }.to raise_error 'bear cannot be both public and protected'
+    end
+
+    it 'does not complain if an entity is only public or only private' do
+      [
+       Joos::Token::Public.new('public', 'hi', 1, 3),
+       Joos::Token::Protected.new('protected', 'hi', 1, 3)
+      ].each do |mod|
+        expect {
+          MTest.new('bear', mod).send(:ensure_only_one_visibility_modifier)
+        }.to_not raise_error
+      end
+    end
+
+    it 'exposes modifiers for the entity through #modifiers' do
+      mods = [Joos::Token::Public.new('public', 'hi', 1, 3),
+              Joos::Token::Static.new('static', 'hi', 1, 3)]
+      test = MTest.new('snake', *mods)
+      expect(test.modifiers).to be == mods
+    end
+  end
+
   describe Joos::Entity::Package do
+    # @todo
   end
 
   describe Joos::Entity::Class do
@@ -51,7 +189,7 @@ describe Joos::Entity do
   describe Joos::Entity::Method do
   end
 
-  describe Joos::Entity::Parameter do
+  describe Joos::Entity::FormalParameter do
   end
 
   describe Joos::Entity::LocalVariable do
