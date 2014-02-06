1b489cc619f698c9d6ffb59a98e03109c526bfcf
Mark Rada <marada@uwaterloo.ca>
2014-02-05 22:05:39 -0500

    Cleanup Joos::Scanner a bit

--- lib/joos/scanner.rb
+++ lib/joos/scanner.rb
@@ -15,27 +15,29 @@ class Joos::Scanner
   def self.scan_file path
     raise "#{path} is a non-existant file" unless File.exists? path
 
-    begin
-      dfa = Joos::ScannerDFA.new
-      state = nil
-      tokens = []
-      line_number = 0
+    dfa = Joos::ScannerDFA.new
+    state = nil
+    tokens = []
+    line_number = 0
 
-      File.readlines(path).each_with_index do |line, index|
-        line_number = index
-        scanner_tokens, state = dfa.tokenize line, state
-        dfa.raise_if_illegal_line_end! state
-        tokens += scanner_tokens.map{ |token| dfa.make_token token, path, line}.compact
-      end
-      dfa.raise_if_illegal_eof! state
-    rescue Joos::CompilerException => e
-      # Add file name and line number to exceptions
-      e.file = path
-      e.line = line_number
-      raise e
+    File.readlines(path).each_with_index do |line, index|
+      line_number = index
+      scanner_tokens, state = dfa.tokenize line, state
+      dfa.raise_if_illegal_line_end! state
+      scanner_tokens.map! { |token|
+        dfa.make_token token, path, line
+      }
+      scanner_tokens.compact!
+      tokens += scanner_tokens
     end
-
+    dfa.raise_if_illegal_eof! state
     tokens
+
+  rescue Joos::CompilerException => e
+    # Add file name and line number to exceptions
+    e.file = path
+    e.line = line_number
+    raise e
   end
 
 end
