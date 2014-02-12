df09752aed3e81f4fb754ac89b1bf259334d0814
Mark Rada <marada@uwaterloo.ca>
2014-02-12 17:05:55 -0500

    Make Token::Identifier errors less redundant
    Redundancy introduced by a recent change in Token#inspect

--- lib/joos/token/identifier.rb
+++ lib/joos/token/identifier.rb
@@ -46,10 +46,8 @@ class Joos::Token::Identifier < Joos::Token
   class BadFirstCharacter < IllegalName
     # @param id [Joos::Token::Identifier]
     def initialize id
-      super <<-EOM
-Identifier #{id.value} @ #{id.source} must start with a letter, underscore,
-or the $ character.
-      EOM
+      super(id.inspect <<
+            'must start with a letter, underscore, or the $ character.')
     end
   end
 
@@ -59,10 +57,9 @@ or the $ character.
   class BadName < IllegalName
     # @param id [Joos::Token::Identifier]
     def initialize id
-      super <<-EOM
-Identifier #{id.value} @ #{id.source} is not a valid identifier name.
-Identifier names must match #{PATTERN}.
-      EOM
+      super(id.inspect <<
+            "is not a valid identifier name.\n" <<
+            "Identifier names must match #{PATTERN}")
     end
   end
 
@@ -73,10 +70,9 @@ Identifier names must match #{PATTERN}.
   class ReservedWord < IllegalName
     # @param id [Joos::Token::Identifier]
     def initialize id
-      super <<-EOM
-Identifier #{id.value} @ #{id.source} is not allowed to be an identifier
-because it is a keyword/operator/separator or otherwise reserved token.
-      EOM
+      super(id.inspect <<
+            'is not allowed to be an identifier becasue it is a ' <<
+            'reserved word or operator.')
     end
   end
 
