27471cff2a5d83ee9cc3e4045f02e9dde309f589
Mark Rada <marada@uwaterloo.ca>
2014-02-15 18:41:04 -0500

    Run stdlib tests as a single thing
    Since we are now doing semantic analysis we can no longer consider the
    files by themselves.

--- test/stdlib_test.rb
+++ test/stdlib_test.rb
@@ -3,14 +3,9 @@ require 'helper'
 ##
 # Joos stdlib Marmoset tests
 class StandardLibraryTests < Minitest::Test
-  parallelize_me!
 
-  Dir.glob('test/stdlib/**/*.java').each do |file|
-    test_case = File.basename(file, '.java')
-
-    define_method "test_#{test_case}" do
-      assert_compile file
-    end
+  def test_stdlib
+    assert_compile Dir.glob('test/stdlib/5.0/**/*.java')
   end
 
 end
