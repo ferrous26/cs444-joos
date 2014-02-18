ca20d63e0680408a57ed19ee8e2da30c3983879b
Mark Rada <marada@uwaterloo.ca>
2014-02-18 03:37:00 -0500

    Collapse comma separated lists of Expression in the AST during building

---
+++ lib/joos/ast/expressions.rb
@@ -0,0 +1,7 @@
+require 'joos/ast'
+
+##
+# A comma separated list of Expression AST nodes
+class Joos::AST::Expressions
+  include ListCollapse
+end
---
+++ spec/ast/expressions_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'joos/ast/expressions'
+
+describe Joos::AST::Expressions do
+
+  it 'should have some passing tests' do
+    expect(true).to be false
+  end
+
+end
