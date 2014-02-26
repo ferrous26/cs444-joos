be186cc53a8da49ad9d2bc4531ac13fc5a815ada
Mark Rada <marada@uwaterloo.ca>
2014-02-26 01:15:50 -0500

    Fix the Je_allthefixings.java test to fail in the correct way
    Now that we are checking constructor names...

--- test/a1/Je_allthefixings.java
+++ test/a1/Je_allthefixings.java
@@ -3,7 +3,7 @@ package Foo;
 import java.util.*;
 
 public abstract class Je_allthefixings extends all implements the.fixings, andMore {
-  J1_allthefixings(int num) {
+  Je_allthefixings(int num) {
   }
 
   abstract int a(int bar);
