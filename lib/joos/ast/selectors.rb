94a3e10876b80be13de2a00d501d3530847d73f0
Mark Rada <marada@uwaterloo.ca>
2014-02-28 00:22:12 -0500

    Allow prepending to a AST::Selectors list

--- lib/joos/ast/selectors.rb
+++ lib/joos/ast/selectors.rb
@@ -1,7 +1,11 @@
 require 'joos/ast'
 
 ##
-# Method/Field selector for a qualified identifiers.
+# A list of method calls
 class Joos::AST::Selectors
   include ListCollapse
+
+  def prepend selector
+    @nodes.unshift selector
+  end
 end
--- spec/ast/selectors_spec.rb
+++ spec/ast/selectors_spec.rb
@@ -3,9 +3,20 @@ require 'joos/ast/selectors'
 
 describe Joos::AST::Selectors do
 
-  it 'mixes in ListCollapse' do
-    ancestors = Joos::AST::Selectors.ancestors
-    expect(ancestors).to include Joos::AST::ListCollapse
+  it 'is list collapsable' do
+    expect(Joos::AST::Selectors.ancestors).to include Joos::AST::ListCollapse
+  end
+
+  it 'allows prepending nodes with #prepend' do
+    selectors = Joos::AST::Selectors.new [:a]
+
+    selectors.prepend :b
+    expect(selectors.first).to be == :b
+    expect(selectors.to_a).to be == [:b, :a]
+
+    selectors.prepend :c
+    expect(selectors.first).to be == :c
+    expect(selectors.to_a).to be == [:c, :b, :a]
   end
 
 end
