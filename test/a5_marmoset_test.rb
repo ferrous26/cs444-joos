48536cab0c5bf5c1edd074839676768c3ef44003
Mark Rada <marada@uwaterloo.ca>
2014-03-01 13:19:02 -0500

    Pseudo-enable the A5 marmoset tests
    This will only test that we are able to compile with crashing, this
    suite still needs to be modified to actually assemble and link the
    produced binary file and then run the program to see if we get the
    expected output.

---
+++ test/a5_marmoset_test.rb
@@ -0,0 +1,56 @@
+require 'helper'
+
+##
+# Assignment 5 Marmoset tests
+class Assignment5Tests < Minitest::Test
+  parallelize_me!
+
+  # only have to define this because we have so many tests
+  def self.test_order
+    :alpha
+  end
+
+  def assert_compile *files
+    super(*files.concat(stdlib))
+  end
+
+  def refute_compile *files
+    super(*files.concat(stdlib))
+  end
+
+  def stdlib
+    @stdlib ||= Dir.glob('test/stdlib/5.0/**/*.java')
+  end
+
+  def self.make_directory_test dir
+    define_method "test_#{dir.split('/').last}" do
+      files = Dir.glob("#{dir}/**/*.java")
+      if dir.split('/').last =~ /\AJe/
+        refute_compile files
+      else
+        assert_compile files
+      end
+    end
+  end
+
+  def self.make_single_file_test file
+    test_case = File.basename(file, '.java')
+
+    define_method "test_#{test_case}" do
+      if test_case =~ /\AJe/
+        refute_compile file
+      else
+        assert_compile file
+      end
+    end
+  end
+
+  Dir.glob('test/a5/*').each do |file|
+    if File.directory? file
+      make_directory_test file
+    elsif file.match(/java$/)
+      make_single_file_test file
+    end
+  end
+
+end
