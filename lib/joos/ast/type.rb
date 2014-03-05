ebf0879cb24b734b3c4f8f1872a42b4b46ce08a3
Mark Rada <marada@uwaterloo.ca>
2014-03-05 16:51:29 -0500

    Cache type resolution of Type AST nodes
    And make inspect output prettier once the type has been resolved.

--- lib/joos/ast/type.rb
+++ lib/joos/ast/type.rb
@@ -6,9 +6,25 @@ require 'joos/version'
 # This can be any Joos type: basic type, array type, or reference type.
 class Joos::AST::Type
 
+  # @return [Joos::BasicType, Joos::Array, Joos::Entity::CompilationUnit]
+  attr_reader :type
+
   # @param env [Joos::Entity::CompilationUnit]
   # @return [Joos::BasicType, Joos::Entity::CompilationUnit, Joos::Array]
   def resolve env
+    @type ||= internal_resolve env
+  end
+
+  ##
+  # Overriden to produce nicer output once the type has been resolved
+  def inspect tab = 0
+    @type ? (taby(tab) << @type.type_inspect) : super
+  end
+
+
+  private
+
+  def internal_resolve env
     if self.BasicType
       Joos::BasicType.new self.BasicType.first
 
@@ -26,7 +42,6 @@ class Joos::AST::Type
 
     else
       raise "Unknown AST::Type type: #{inspect}"
-
     end
   end
 
