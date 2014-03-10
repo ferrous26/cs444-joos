47af3f0d72b695fe76af54f87ddd8bdc8614a88f
Mark Rada <marada@uwaterloo.ca>
2014-03-10 00:16:43 -0400

    Temporary hack to add length field to arrays

--- lib/joos/array.rb
+++ lib/joos/array.rb
@@ -23,6 +23,15 @@ class Joos::Array
     self.type == other.type if other.respond_to? :type
   end
 
+  # @todo MAKE THIS WAY LESS OF A HACK
+  FIELD = Object.new
+  FIELD.define_singleton_method(:name) { Joos::Token.make :Identifier, 'length' }
+  FIELD.define_singleton_method(:type) { Joos::BasicType.new :Int }
+
+  def all_fields
+    [FIELD]
+  end
+
 
   # @!group Type API
 
