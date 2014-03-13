7ceb58778dcffc4ef016639085c2b8b683ed17de
Mark Rada <marada@uwaterloo.ca>
2014-03-13 13:10:51 -0400

    Implement GH-106

--- lib/joos/entity/constructor.rb
+++ lib/joos/entity/constructor.rb
@@ -9,6 +9,20 @@ require 'joos/entity/modifiable'
 class Joos::Entity::Constructor < Joos::Entity::Method
   include Modifiable
 
+  # @return [Joos::Entity::Constructor]
+  attr_reader :superconstructor
+
+  ##
+  # Exception raised when the super class of a class does not contain a default
+  # constructor. The default constructor is required for the implicity super
+  # constructor call that all subclasses make during instance construction.
+  class NoSuperConstructor < Joos::CompilerException
+    def initialize supa, sub
+      msg = "#{supa.inspect} requires a default constructor for #{sub.inspect}"
+      super msg, sub
+    end
+  end
+
   # @param node [Joos::AST::ClassBodyDeclaration]
   # @param klass [Joos::AST::Class]
   def initialize node, klass
@@ -24,6 +38,26 @@ class Joos::Entity::Constructor < Joos::Entity::Method
   def validate
     ensure_modifiers_not_present(:Static, :Abstract, :Final, :Native)
     super # super called here to null out some superclass checks
+  end
+
+  def type_check
+    check_that_implicit_superconstructor_exists
+    super
+  end
+
+
+  private
+
+  def check_that_implicit_superconstructor_exists
+    supa = type_environment.superclass
+
+    # java.lang.Object is exempt from this rule
+    return unless supa && !supa.top_class?
+
+    @superconstructor = supa.constructors.find { |c| c.parameters.empty? }
+    unless @superconstructor
+      raise NoSuperConstructor.new(supa, type_environment)
+    end
   end
 
 end
