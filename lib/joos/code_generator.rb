f2793d656739065dc3ef89864ecb73072a1865ae
Mark Rada <marada@uwaterloo.ca>
2014-03-28 16:37:43 -0400

    Add code generation functionality for extern symbols and literal strings

--- lib/joos/code_generator.rb
+++ lib/joos/code_generator.rb
@@ -1,3 +1,5 @@
+require 'base64'
+require 'digest/md5'
 require 'joos/utilities'
 
 
@@ -68,6 +70,16 @@ class Joos::CodeGenerator
     end
   end
 
+
+  ##
+  # Symbols that are externally defined and should be declared as such
+  #
+  # @return [Array<String>]
+  attr_reader :symbols
+
+  # @return [Hash]
+  attr_reader :strings
+
   # @param unit      [Joos::Entity::CompilationUnit]
   # @param platform  [Symbol] pretty much has to be `:i386`
   # @param directory [String] where to put all the asm
@@ -76,6 +88,8 @@ class Joos::CodeGenerator
     @unit     = unit
     @file     = unit.fully_qualified_name.join('_') << '.s'
     @fd       = File.open "#{directory}/#{@file}", 'w'
+    @symbols  = ['__debexit', '__malloc', '__exception']
+    @strings  = literal_string_hash
   end
 
   if Joos::Utilities.darwin?
@@ -89,21 +103,24 @@ class Joos::CodeGenerator
   end
 
   def generate_data
+    @fd.puts 'section .data'
+
     # tag data
     # vtable
     # static fields
+
+    # literal strings
   end
 
   def generate_text
+    @fd.puts 'section .text'
+
     # static methods
     # instance methods
   end
 
   def generate_main
-    @main = true
     @fd.puts <<-EOC
-extern __debexit
-
 global #{start_sym}
 #{start_sym}:
     mov eax, 123
@@ -112,7 +129,41 @@ global #{start_sym}
   end
 
   def finalize
+    append_literal_strings
+    append_external_symbols
     @fd.close
+  end
+
+
+  private
+
+  def append_literal_strings
+    @fd.puts
+    @fd.puts ';; Literal strings'
+    @fd.puts 'section .data'
+    @strings.each_pair do |str, symbol|
+      @fd.puts "#{symbol}: db '#{str}'"
+    end
+  end
+
+  def append_external_symbols
+    @fd.puts
+    @fd.puts ';; Symbols that we need to import'
+    @symbols.each do |symbol|
+      @fd.puts "extern #{symbol}"
+    end
+  end
+
+  def literal_string_hash
+    @strings = Hash.new do |hash, key|
+      digest = Digest::MD5.digest key
+      encode = Base64.encode64 digest
+      encode.chomp!
+      encode.gsub!(/\+/, '$')
+      encode.gsub!(/\//, '?')
+      encode.gsub!(/==/, '')
+      hash[key] = "string##{encode}"
+    end
   end
 
 end
