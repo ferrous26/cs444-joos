e3411cad1c54779909b02722a23c01a2a024bfbc
Michael Forsyth <mike.forsyth23@gmail.com>
2014-03-31 14:32:51 -0400

    Fix Mark's test

--- test/a5/J1_superFieldAccess2/Main.java
+++ test/a5/J1_superFieldAccess2/Main.java
@@ -2,13 +2,13 @@ public class Main extends A {
   public Main() {
   }
 
-  public int test() {
+  public int test2() {
     return ((A)this).a;
   }
 
   public int a = 22;
 
   public static int test() {
-    return (new Main()).test();
+    return (new Main()).test2();
   }
 }
