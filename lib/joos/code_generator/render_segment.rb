c1a32d404b97b7340dd147a41907b9fef07e58d7
Riel Fuller <riel.fuller@gmail.com>
2014-04-03 23:09:39 -0400

    J1_Hello that looks vaguely right. Maybe.

--- lib/joos/code_generator/render_segment.rb
+++ lib/joos/code_generator/render_segment.rb
@@ -19,12 +19,15 @@ class Joos::CodeGenerator
       output "#{block.name}:"
 
       block.instructions.each do |instruction|
+        output ";;  #{instruction}"
+
         # Magic
+        @current_instruction = instruction
         handler = self.class.instruction_handlers[instruction.class]
         if handler
           self.instance_exec instruction, &handler
         else
-          output "nop\t\t; #{instruction}"
+          not_implemented
         end
       end
 
@@ -33,7 +36,7 @@ class Joos::CodeGenerator
 
     output
     output '.epilogue:'
-    output "        add esp, #{@allocater.stack_size}"
+    output "        add esp, #{@allocator.stack_size}"
     output '        pop ebp'
 
     @output_instructions
@@ -58,11 +61,33 @@ class Joos::CodeGenerator
     @output_instructions << assembly.decolour
   end
 
+  def not_implemented
+    output ';; Not implemented'
+    destination @current_instruction
+  end
+
   # Get or generate the location of an SSA variable
   # @param instruction [Joos::SSA::Instruction]
   def locate instruction
-    # stub
-    'eax'
+    @allocator.find instruction.target
+  end
+
+  # Where to write a new SSA variable to
+  # @param instruction [Joos::SSA::Instruction]
+  def destination instruction
+    ret = @allocator.allocate instruction.target
+    unless ret
+      output 'push dword 0'
+      ret = @allocator.find instruction.target
+    end
+
+    ret
+  end
+
+  # Claim eax for nefarious purposes. A striaghtforward call in to the RA.
+  # @param instruction [Joos::SSA::Instruction]
+  def take_eax instruction
+    @allocator.take :eax, instruction.target
   end
 
   class << self
@@ -75,6 +100,7 @@ class Joos::CodeGenerator
       @instruction_handlers[type] = Proc.new &block
       return
     end
+
   end
 
 
@@ -88,13 +114,14 @@ class Joos::CodeGenerator
     when Joos::SSA::Return
       if continuation.value
         loc = locate continuation.value
-        output "mov eax, #{loc}"
+        output "mov eax, dword #{loc}"
       end
       output 'jmp .epilogue' if next_block
     when Joos::SSA::Next
       output "jmp #{continuation.block.name}" unless continuation.block == next_block
     when Joos::SSA::Loop
       output "jmp #{continuation.block.name}"
+  # @param instruction [Joos::SSA::Instruction]
     when Joos::SSA::Branch
       # TODO
     when nil
@@ -103,14 +130,13 @@ class Joos::CodeGenerator
   end
 
   instruction Joos::SSA::Const do |ins|
-    loc = locate ins
+    dest = destination ins
     val = if ins.target_type.boolean_type?
             ins.value.to_s
           elsif ins.target_type.basic_type?
             ins.value.to_i
           elsif ins.target_type.string_class?
-            # TODO
-            'null'
+            strings[ins.token.to_binary]
           else
             # Any other reference type can only be null
             if ins.value != 'null'
@@ -118,7 +144,44 @@ class Joos::CodeGenerator
             end
             'null'
           end
-    output "mov #{loc}, #{val}\t; #{ins}"
+    output "mov #{dest}, dword #{val}"
+  end
+
+  instruction Joos::SSA::Get do |ins|
+    case ins.entity
+    when Joos::Entity::Field
+      # static field access
+      label = ins.entity.label
+
+      symbols << label unless ins.entity.type_environment == @unit
+      output "mov #{destination ins}, dword #{label}"
+    when Joos::Entity::LocalVariable
+      not_implemented
+    when Joos::Entity::FormalParameter
+      not_implemented
+    end
+  end
+
+  instruction Joos::SSA::CallMethod do |ins|
+    # Allocate space for destination and claim eax
+    dest = destination ins
+    take_eax ins
+
+    # Save registers
+    @allocator.caller_save
+    
+    # Push arguments and receiver
+    ins.arguments[1..-1].each do |arg|
+      output "push dword #{locate arg}"
+    end
+    output "push dword #{locate ins.receiver}"
+
+    # Call. Result is moved into eax.
+    output "mov eax, #{ins.entity.method_number}"
+    output "call __dispatch"
+    
+    # Pop arguments and receiver
+    output "add esp, [4*#{ins.arguments.length}]"
   end
 
 end
